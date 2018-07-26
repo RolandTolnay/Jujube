453f10e4f5277f55f79a5a86501c070f960b8a55
Roland Tolnay <roland.tolnay@iquestgroup.com>
2018-07-26 18:01:15 +0300

    Fixed url crash

--- Jujube/View/AnalysisCell.swift
+++ Jujube/View/AnalysisCell.swift
@@ -31,10 +31,13 @@ class AnalysisCell: UITableViewCell {
     }
     
     private func downloadImageForActor(_ actor: String) {
-        
-        let url = URL(string: "https://www.googleapis.com/customsearch/v1?q=\(actor)&searchType=image&key=AIzaSyAyW6X5Bh-JHxkYyQmcwS6URlsR-dU24xc&cx=017426432613049717893:d1u8bllqhjo")
+
+      let urlString = "https://www.googleapis.com/customsearch/v1?q=\(actor)&searchType=image&key=AIzaSyAyW6X5Bh-JHxkYyQmcwS6URlsR-dU24xc&cx=017426432613049717893:d1u8bllqhjo"
         let urlSession = Foundation.URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main)
-        let dataTask = urlSession.dataTask(with: url!) { (data, response, error) in
+
+      guard let url = URL(string: urlString) else { return }
+
+        let dataTask = urlSession.dataTask(with: url) { (data, response, error) in
             if let response = response as? HTTPURLResponse {
                 if let data = data, response.statusCode == 200 {
                     do {

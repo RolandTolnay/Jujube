206841cee15279e35541dcb61e0197fb93052d5a
Roland Tolnay <roland.tolnay@iquestgroup.com>
2018-07-26 09:26:39 +0300

    Initial commit

---
+++ Jujube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jujube
+//
+//  Created by Roland Tolnay on 26/07/2018.
+//  Copyright © 2018 iQuest Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+

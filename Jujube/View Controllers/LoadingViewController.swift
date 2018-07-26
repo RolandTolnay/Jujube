b8e2be5d3177bda39f9efa20e30f45165d8ca97f
Roland Tolnay <roland.tolnay@iquestgroup.com>
2018-07-26 18:55:46 +0300

    Fixed loading indicator centering

--- Jujube/View Controllers/LoadingViewController.swift
+++ Jujube/View Controllers/LoadingViewController.swift
@@ -15,9 +15,6 @@ class LoadingViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    let circleLoader = LiquidLoader(frame: loadingView.frame, effect: .growCircle(#colorLiteral(red: 0.8121558428, green: 0.338460505, blue: 0, alpha: 1), 8, 1.2, #colorLiteral(red: 0.9925034642, green: 0.8121734858, blue: 0, alpha: 1)))
-    view.addSubview(circleLoader)
     
     InstagramService.shared.latestImages(completion: { (analyzedActors) in
 
@@ -29,5 +26,12 @@ class LoadingViewController: UIViewController {
       BestPicAlgorithm.shared.setup(withActors: analyzedActors)
       self.dismiss(animated: true, completion: nil)
     })
+  }
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+
+    let circleLoader = LiquidLoader(frame: loadingView.frame, effect: .growCircle(#colorLiteral(red: 0.8121558428, green: 0.338460505, blue: 0, alpha: 1), 8, 1.2, #colorLiteral(red: 0.9925034642, green: 0.8121734858, blue: 0, alpha: 1)))
+    view.addSubview(circleLoader)
   }
 }

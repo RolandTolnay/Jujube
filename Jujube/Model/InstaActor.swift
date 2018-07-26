234d97626f343b2ce4b39602cdb0bb8b1a6b163a
Roland Tolnay <roland.tolnay@iquestgroup.com>
2018-07-26 10:53:51 +0300

    Added InstaActor model

---
+++ Jujube/Model/InstaActor.swift
@@ -0,0 +1,15 @@
+//
+//  InstaActor.swift
+//  Jujube
+//
+//  Created by Roland Tolnay on 26/07/2018.
+//  Copyright © 2018 iQuest Technologies. All rights reserved.
+//
+
+import Foundation
+
+struct InstaActor {
+
+  let object: String
+  let likeCount: Int
+}
--- Jujube/View Controllers/MainViewController.swift
+++ Jujube/View Controllers/MainViewController.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+  @IBOutlet weak var collectionView: UICollectionView!
+
+  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+}
+
+extension MainViewController: UICollectionViewDataSource {
+
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+    return 6
   }
 
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+    guard let cell = collectionView.dequeueReusableCell(with: PhotoCell.self, for: indexPath)
+      else { return UICollectionViewCell() }
+
+
+    return cell
+  }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    // TODO: Try to upload photo to instagram
+  }
 }
